b15c122e693653693235d6a476a324a8187de3f9
iddqd2d <iddqd2d@gmail.com>
2019-03-10 10:22:47 +0200

    fixed

--- src/calculator/Calc.java
+++ src/calculator/Calc.java
@@ -2,12 +2,13 @@ package calculator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.DoubleBinaryOperator;
 
 public class Calc {
-    private Map<Character, DoubleBinaryOperator> operation;
+    private static Map<Character, DoubleBinaryOperator> operation;
 
-    Calc() {
+    static  {
         operation = new HashMap<>();
         operation.put('+', Double::sum);
         operation.put('-', (a, b) -> a - b);
@@ -16,10 +17,11 @@ public class Calc {
         operation.put('^', Math::pow);
         operation.put('r', (a, b) -> Math.exp(Math.log(a) / b));
         operation.put('x', (a, b) -> Math.pow(((a + b) / (a + 117)), b));
+        operation.put('n', (a, b) -> 0);
     }
 
-    public double calculate(double num1, double num2, char operator) {
-        return operation.get(operator).applyAsDouble(num1, num2);
+    public static double calculate(double num1, double num2, char operator) {
+        return operation.getOrDefault(operator, operation.get('n')).applyAsDouble(num1, num2);
     }
 
 }
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,14 +1,16 @@
 package calculator;
 
+import static calculator.Calc.calculate;
+
 public class Main {
     public static void main(String[] args) {
-        Calc calc = new Calc();
-        System.out.println(calc.calculate(2, 1, '+'));
-        System.out.println(calc.calculate(2, 1, '-'));
-        System.out.println(calc.calculate(2, 2, '*'));
-        System.out.println(calc.calculate(1, 0, '/'));
-        System.out.println(calc.calculate(2, 2, 'r'));
-        System.out.println(calc.calculate(8, 2, '^'));
-        System.out.println(calc.calculate(2, 2, 'x'));
+        System.out.println(calculate(2, 1, '+'));
+        System.out.println(calculate(2, 1, '-'));
+        System.out.println(calculate(2, 2, '*'));
+        System.out.println(calculate(1, 0, '/'));
+        System.out.println(calculate(2, 2, 'r'));
+        System.out.println(calculate(8, 2, '^'));
+        System.out.println(calculate(2, 2, 'x'));
+        System.out.println(calculate(2, 2, ' '));
     }
 }
